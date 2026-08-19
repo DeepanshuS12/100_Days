edc961e9dcaedfd7e3abf7f9cb4934d9c3b5928c
Deepanshu <deepanshu112000singh@gmail.com>
2026-08-19 20:59:57 +0530

    Day 10

--- triangle.c
+++ triangle.c
@@ -1,4 +1,4 @@
-//Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+// Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
 
 /*
 Sample Test Cases:
@@ -19,20 +19,23 @@ Scalene
 
 */
 #include <stdio.h>
-int main() {
-    int a,b,c;
+int main()
+{
+    int a, b, c;
     printf("ENTER SIDES OF A TRIANGLE : ");
-    scanf("%d %d %d",&a ,&b ,&c);
-    if(a+b>c || b+c>a || a+c>b){
-        if(a==b || b==c || a==c)
-        printf(" IT IS AN ISOSCELES TRIANGLE \n");
-        if(a!=b && b!=c && a!=c)
-        printf(" IT IS A SCALENE TRIANGLE \n");
-        if (a==b && b==c)
-        printf(" IT IS AN EQUILATERAL TRIANGLE \n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a + b > c || b + c > a || a + c > b)
+    {
+        if (a == b || b == c || a == c)
+            printf(" IT IS AN ISOSCELES TRIANGLE \n");
+        if (a != b && b != c && a != c)
+            printf(" IT IS A SCALENE TRIANGLE \n");
+        if (a == b && b == c)
+            printf(" IT IS AN EQUILATERAL TRIANGLE \n");
     }
-    else{
+    else
+    {
         printf(" IT IS AN INVALID TRIANGLE");
     }
-     return 0;
+    return 0;
 }
