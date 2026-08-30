3f977bbd44c0cdaa77dbdca73c92726813872cf6
Deepanshu <deepanshu112000singh@gmail.com>
2026-08-30 09:48:27 +0530

    Day 14

---
+++ sum_of_odd.c
@@ -0,0 +1,27 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+Output 1:
+9
+
+Input 2:
+5
+Output 2:
+25
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n,sum=0;
+    printf("ENTER THE NUMBER OF ODD NUMBERS FOR SUMATION : ");
+    scanf("%d",&n);
+    for(int i=1;i<=(n*2);i+=2){
+        sum+=i;
+    }
+    printf("Sum : %d", sum);
+    return 0;
+}
